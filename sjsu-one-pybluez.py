287fc98b7709c5ead43d43d9c51aaec30b3c2136
Matthew Carlis <matthew.carlis@sjsu.edu>
2015-05-31 23:15:00 -0700

    Commit stable sjsu-one-pybluez.py
    Handles the closing of the socket properly.

--- sjsu-one-pybluez.py
+++ sjsu-one-pybluez.py
@@ -1,13 +1,14 @@
 import serial
+import socket
 import string
 import time
 import bluetooth
 
-bt_mac = 'ur bt device mac address'
+bt_mac = '00:06:66:6C:60:E1'
 bt_port = 1
 
 def connect():
-    while(True):    
+    while(True):
         try:
             bt_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
             bt_sock.connect((bt_mac, bt_port))
@@ -18,16 +19,29 @@ def connect():
             time.sleep(2)
     return bt_sock;
 bt_sock = connect()
+
+def _teardown():
+    bt_sock.shutdown(2)
+    try:
+        bt_sock.recv(1024, socket.MSG_DONTWAIT)
+    except:
+        pass
+    bt_sock.close()
+
 while(True):
     try:
         send_data = raw_input('sjsu-one$ ')
         bt_sock.sendall(send_data + '\r\n')
-        print bt_sock.recv(128)
-        print bt_sock.recv(128)
-        print bt_sock.recv(128)
+        print bt_sock.recv(1024)
+        print bt_sock.recv(1024)
+        print bt_sock.recv(1024)
+    except KeyboardInterrupt:
+        _teardown()
+        break
     except bluetooth.btcommon.BluetoothError as error:
         print "Caught BluetoothError: ", error
         time.sleep(3)
         bt_sock = connect()
-        pass
-bt_sock.close()
+    except Exception:
+        _teardown()
+        break
